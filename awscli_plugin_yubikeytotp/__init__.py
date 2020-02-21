186ff0961ea23c9dd8b03bf8f6e0b9c00e8bbc86
Thomas Liebetraut <thomas@tommie-lie.de>
2020-02-21 12:33:26 +0100

    acquire MFA serial number

--- awscli_plugin_yubikeytotp/__init__.py
+++ awscli_plugin_yubikeytotp/__init__.py
@@ -11,6 +11,13 @@ def inject_yubikey_totp_prompter(session, **kwargs):
         providers = session.get_component("credential_provider")
     except ProfileNotFound:
         return
+
+    config = session.get_scoped_config()
+    mfa_serial = config.get("mfa_serial")
+    if mfa_serial is None:
+        # no MFA, so don't interfere with regular flow
+        return
+
     assume_role_provider = providers.get_provider("assume-role")
     assume_role_provider._prompter = yubikey_totp_prompter
 
