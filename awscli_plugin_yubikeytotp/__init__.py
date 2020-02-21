19468da89ecfe810da87b9b61a5de2e3997ab252
Thomas Liebetraut <thomas@tommie-lie.de>
2020-02-21 12:05:14 +0100

    hook into AssumeRoleProvider and replace existing prompter

--- awscli_plugin_yubikeytotp/__init__.py
+++ awscli_plugin_yubikeytotp/__init__.py
@@ -1,2 +1,23 @@
+from botocore.exceptions import ProfileNotFound
+import getpass
+
+
+def yubikey_totp_prompter(prompt):
+    return getpass.getpass(prompt)
+
+
+def inject_yubikey_totp_prompter(session, **kwargs):
+    try:
+        providers = session.get_component("credential_provider")
+    except ProfileNotFound:
+        return
+    assume_role_provider = providers.get_provider("assume-role")
+    assume_role_provider._prompter = yubikey_totp_prompter
+
+
 def awscli_initialize(cli):
-    print("Yubikey Plugin initialized")
+    cli.register(
+        "session-initialized",
+        inject_yubikey_totp_prompter,
+        unique_id="inject_yubikey_totp_provider",
+    )
