420b70e0974fd1db73f4cc98fdac740c978d1190
Thomas Liebetraut <thomas@tommie-lie.de>
2020-02-21 16:31:08 +0100

    call ykman to generate OATH-TOTP code

--- awscli_plugin_yubikeytotp/__init__.py
+++ awscli_plugin_yubikeytotp/__init__.py
@@ -1,5 +1,6 @@
 from botocore.exceptions import ProfileNotFound
-import getpass
+import subprocess
+import sys
 
 
 class YubikeyTotpPrompter(object):
@@ -7,7 +8,29 @@ class YubikeyTotpPrompter(object):
         self.mfa_serial = mfa_serial
 
     def __call__(self, prompt):
-        return getpass.getpass(prompt)
+        available_keys_result = subprocess.run(
+            ["ykman", "oath", "list"], capture_output=True
+        )
+        if available_keys_result.returncode != 0:
+            print("No YubiKey found.", file=sys.stderr)
+            return
+
+        available_keys = available_keys_result.stdout.decode("utf-8").split()
+        if not self.mfa_serial in available_keys:
+            print(
+                "The requested mfa_serial has no OATH credentials stored on your YubiKey.",
+                file=sys.stderr,
+            )
+            return
+
+        print(
+            "Generating OATH code on YubiKey. You may have to touch your YubiKey to proceed..."
+        )
+        ykman_result = subprocess.run(
+            ["ykman", "oath", "code", "-s", self.mfa_serial], capture_output=True
+        )
+        token = ykman_result.stdout.decode("utf-8").strip()
+        return token
 
 
 def inject_yubikey_totp_prompter(session, **kwargs):
