1da769e47a5c7fff2af7125b8ed870af717a25ef
Thomas Liebetraut <thomas@tommie-lie.de>
2020-02-23 17:36:42 +0100

    fallback to original prompter if Yubikey is not found or key is not found on Yubikey (fixes #2)

--- awscli_plugin_yubikeytotp/__init__.py
+++ awscli_plugin_yubikeytotp/__init__.py
@@ -4,33 +4,39 @@ import sys
 
 
 class YubikeyTotpPrompter(object):
-    def __init__(self, mfa_serial):
+    def __init__(self, mfa_serial, original_prompter=None):
         self.mfa_serial = mfa_serial
+        self._original_prompter = original_prompter
 
     def __call__(self, prompt):
-        available_keys_result = subprocess.run(
-            ["ykman", "oath", "list"], capture_output=True
-        )
-        if available_keys_result.returncode != 0:
-            print("No YubiKey found.", file=sys.stderr)
-            return
+        try:
+            available_keys_result = subprocess.run(
+                ["ykman", "oath", "list"], capture_output=True, check=True
+            )
+            available_keys = available_keys_result.stdout.decode("utf-8").split()
+            available_keys.index(self.mfa_serial)
 
-        available_keys = available_keys_result.stdout.decode("utf-8").split()
-        if not self.mfa_serial in available_keys:
+            print(
+                "Generating OATH code on YubiKey. You may have to touch your YubiKey to proceed..."
+            )
+            ykman_result = subprocess.run(
+                ["ykman", "oath", "code", "-s", self.mfa_serial], capture_output=True
+            )
+            print("Successfully created OATH code.")
+            token = ykman_result.stdout.decode("utf-8").strip()
+            return token
+        except subprocess.CalledProcessError as e:
+            print("No YubiKey found.", file=sys.stderr)
+        except ValueError as e:
             print(
                 "The requested mfa_serial has no OATH credentials stored on your YubiKey.",
                 file=sys.stderr,
             )
-            return
 
-        print(
-            "Generating OATH code on YubiKey. You may have to touch your YubiKey to proceed..."
-        )
-        ykman_result = subprocess.run(
-            ["ykman", "oath", "code", "-s", self.mfa_serial], capture_output=True
-        )
-        token = ykman_result.stdout.decode("utf-8").strip()
-        return token
+        if self._original_prompter:
+            return self._original_prompter(prompt)
+
+        return None
 
 
 def inject_yubikey_totp_prompter(session, **kwargs):
@@ -46,7 +52,10 @@ def inject_yubikey_totp_prompter(session, **kwargs):
         return
 
     assume_role_provider = providers.get_provider("assume-role")
-    assume_role_provider._prompter = YubikeyTotpPrompter(mfa_serial)
+    original_prompter = assume_role_provider._prompter
+    assume_role_provider._prompter = YubikeyTotpPrompter(
+        mfa_serial, original_prompter=original_prompter
+    )
 
 
 def awscli_initialize(cli):
